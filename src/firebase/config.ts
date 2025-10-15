f3c3d10edccb224741a18bad39152ab3be710c49
Nyall Prospere <Nyall2006@gmail.com>
2025-10-15 15:41:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6540754669-81720",
+  "appId": "1:492558291709:web:9019e178485dcc1e056494",
+  "apiKey": "AIzaSyDVjD_gBD7frapVAGg-1mJgHCMpOd8Fevk",
+  "authDomain": "studio-6540754669-81720.firebaseapp.com",
+  "measurementId": "G-W3S20HV5RX",
+  "messagingSenderId": "492558291709"
+};
