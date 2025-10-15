7a284bb78537eb5aa9837b129fe43ed11e42c801
Nyall Prospere <Nyall2006@gmail.com>
2025-10-15 16:52:55 +0000

    Make sure files are being uploaded to this bucket: gs://studio-654075466

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6540754669-81720",
   "appId": "1:492558291709:web:9019e178485dcc1e056494",
-  "storageBucket": "studio-6540754669-81720.appspot.com",
+  "storageBucket": "studio-6540754669-81720.firebasestorage.app",
   "apiKey": "AIzaSyDVjD_gBD7frapVAGg-1mJgHCMpOd8Fevk",
   "authDomain": "studio-6540754669-81720.firebaseapp.com",
   "measurementId": "G-W3S20HV5RX",
