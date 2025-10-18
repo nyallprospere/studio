1c7cde6837a8f0af06dba18673167f2ae83714a4
Nyall Prospere <Nyall2006@gmail.com>
2025-10-18 01:33:02 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/draggable-image-overlay.tsx
+++ src/components/draggable-image-overlay.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { DndContext, useDraggable, DragEndEvent } from '@dnd-kit/core';
 import Image from 'next/image';
 
