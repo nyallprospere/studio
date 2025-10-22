9fc11fca411443db9b64bb6dd35b5f00de9a82a4
Nyall Prospere <Nyall2006@gmail.com>
2025-10-22 08:20:09 +0000

    Make this text green (_for element span_)

--- src/components/constituency-popover-content.tsx
+++ src/components/constituency-popover-content.tsx
@@ -102,7 +102,7 @@ function CandidateBox({
                                 {isStriped && barFill === 'blue-red-stripes' && <div className="absolute inset-0 red-stripes-overlay"></div>}
                             </div>
                             <div className="absolute inset-0 flex items-center justify-between px-2">
-                                <span className={cn("font-bold text-xs", party?.acronym === 'UWP' ? 'text-black' : 'text-white')}>
+                                <span className={cn("font-bold text-xs", party?.acronym === 'UWP' ? 'text-green-600' : 'text-white')}>
                                     {votes?.toLocaleString()}
                                     {isWinner && margin ? <sup className="font-semibold"> (+{margin.toLocaleString()})</sup> : null}
                                 </span>
