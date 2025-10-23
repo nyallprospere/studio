36e601d259c8091c523e1a606c22a55b80cc4802
Nyall Prospere <Nyall2006@gmail.com>
2025-10-23 12:12:19 +0000

    remove this  (_for element <Button>_)

--- src/app/admin/news/page.tsx
+++ src/app/admin/news/page.tsx
@@ -233,42 +233,6 @@ export default function AdminNewsPage() {
                         <SelectItem value="year">Year</SelectItem>
                     </SelectContent>
                 </Select>
-                 <Popover>
-                    <PopoverTrigger asChild>
-                        <Button
-                        id="date"
-                        variant={"outline"}
-                        className={cn(
-                            "w-[260px] justify-start text-left font-normal",
-                            !date && "text-muted-foreground"
-                        )}
-                        >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date?.from ? (
-                            date.to ? (
-                            <>
-                                {format(date.from, "LLL dd, y")} -{" "}
-                                {format(date.to, "LLL dd, y")}
-                            </>
-                            ) : (
-                            format(date.from, "LLL dd, y")
-                            )
-                        ) : (
-                            <span>Custom Range</span>
-                        )}
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="end">
-                        <Calendar
-                        initialFocus
-                        mode="range"
-                        defaultMonth={date?.from}
-                        selected={date}
-                        onSelect={(range) => { setDate(range); setDatePreset('custom'); }}
-                        numberOfMonths={2}
-                        />
-                    </PopoverContent>
-                </Popover>
               </div>
             </div>
           </CardHeader>
