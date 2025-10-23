dd50d2f088d883c175240620c596aa48718c2c70
Nyall Prospere <Nyall2006@gmail.com>
2025-10-23 11:58:13 +0000

    remove this (_for element <Button>_)

--- src/app/admin/analytics/data-table.tsx
+++ src/app/admin/analytics/data-table.tsx
@@ -151,42 +151,6 @@ export function DataTable<TData, TValue>({
                         <SelectItem value="year">Year</SelectItem>
                     </SelectContent>
                 </Select>
-                 <Popover>
-                    <PopoverTrigger asChild>
-                        <Button
-                        id="date"
-                        variant={"outline"}
-                        className={cn(
-                            "w-[300px] justify-start text-left font-normal",
-                            !date && "text-muted-foreground"
-                        )}
-                        >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date?.from ? (
-                            date.to ? (
-                            <>
-                                {format(date.from, "LLL dd, y")} -{" "}
-                                {format(date.to, "LLL dd, y")}
-                            </>
-                            ) : (
-                            format(date.from, "LLL dd, y")
-                            )
-                        ) : (
-                            <span>Custom Range</span>
-                        )}
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="end">
-                        <Calendar
-                        initialFocus
-                        mode="range"
-                        defaultMonth={date?.from}
-                        selected={date}
-                        onSelect={(range) => { setDate(range); setDatePreset('custom'); }}
-                        numberOfMonths={2}
-                        />
-                    </PopoverContent>
-                </Popover>
                  <Button variant="outline" onClick={() => handleExport('xlsx')}>
                     <Download className="mr-2 h-4 w-4" /> Export
                  </Button>
