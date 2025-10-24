1c1b0dcfcf9a2f893daa3f32a4d4b020285692ad
Nyall Prospere <Nyall2006@gmail.com>
2025-10-24 07:51:30 +0000

    Try fixing this error: `Console Error: FirebaseError: Missing or insuffi

--- src/app/admin/logos/logo-upload-dialog.tsx
+++ src/app/admin/logos/logo-upload-dialog.tsx
@@ -195,7 +195,6 @@ export function LogoUploadDialog({ isOpen, onClose, party, elections, constituen
                 requestResourceData: values,
             });
             errorEmitter.emit('permission-error', permissionError);
-            throw error;
         });
 
         onSuccess();
