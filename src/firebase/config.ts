e96938216c55669376e4cc420f49f6bfbf15bac8
Nyall Prospere <Nyall2006@gmail.com>
2025-11-12 07:23:27 +0000

    set all STORAGE connections to upload to gs://studio-6540754669-81720.fi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6540754669-81720",
   "appId": "1:492558291709:web:0015f836e22c8c01056494",
-  "storageBucket": "studio-6540754669-81720.appspot.com",
+  "storageBucket": "studio-6540754669-81720.firebasestorage.app",
   "apiKey": "AIzaSyDVjD_gBD7frapVAGg-1mJgHCMpOd8Fevk",
   "authDomain": "studio-6540754669-81720.firebaseapp.com",
   "messagingSenderId": "492558291709"
